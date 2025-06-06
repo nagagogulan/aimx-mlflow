06a70e1969b3da61f707904af05a43943e50c159
Sugumaran <sugumaran.dhayalan@twilightitsolutions.com>
2025-06-06 18:04:53 +0530

    fix: path issue

--- temporal/src/activities/evaluation.js
+++ temporal/src/activities/evaluation.js
@@ -12,9 +12,7 @@ const __dirname = path.dirname(__filename);
 console.log("EVALUATION ACTIVITY: ", __dirname);
 console.log("EVALUATION_BASE_DIR:", __filename);
 
-
-// Dynamically resolve path up to '/temporal'
-const projectRoot = path.resolve(__dirname, '../..'); // ✅ correct
+const projectRoot = "/usr/src/app"; // ✅ Container-based
  
 // Now reference your target path relative to project root
 const INFERENCE_BASE_DIR = path.join(projectRoot, 'scripts', 'text-classification', 'distilbert', 'pkl');
@@ -105,9 +103,10 @@ export async function copyInferenceScripts(options) {
   const datasetFileName = DATASET_URL.Value.split("/").pop();
   console.log("modelFileName: ", modelFileName);
   console.log("datasetFileName: ", datasetFileName);
-  const modelWeightFullPath = `app/${MODEL_WEIGHT_URL.path}`;
+  const modelWeightFullPath = path.join("app/", MODEL_WEIGHT_URL.path);
+  const datasetFullPath = path.join("app/", DATASET_URL.Value);
+
   console.log("modelWeightFullPath: ", modelWeightFullPath);
-  const datasetFullPath = `app/${DATASET_URL.Value}`;
   console.log("datasetFullPath: ", datasetFullPath);
 
 
